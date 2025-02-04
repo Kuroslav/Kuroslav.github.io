de8f1395d1c059ae9da6daf7d11cf6c045368fd9
Zdeněk Horák <horkys12@seznam.cz>
2025-02-04 15:53:29 +0100

    MOSHI-RESPONSIBLE-UPDATE

--- moshi/navbar.js
+++ moshi/navbar.js
@@ -1,19 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const nav = document.querySelector(".nav"),
-        searchIcon = document.querySelector("#searchIcon"),
         navOpenBtn = document.querySelector(".navOpenBtn"),
         navCloseBtn = document.querySelector(".navCloseBtn");
-
-    if (searchIcon) {
-        searchIcon.addEventListener("click", () => {
-            nav.classList.toggle("openSearch");
-            nav.classList.remove("openNav");
-            if (nav.classList.contains("openSearch")) {
-                return searchIcon.classList.replace("uil-search", "uil-times");
-            }
-            searchIcon.classList.replace("uil-times", "uil-search");
-        });
-    }
 
     if (navOpenBtn) {
         navOpenBtn.addEventListener("click", () => {
